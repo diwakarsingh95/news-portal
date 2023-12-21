fb4149b7a8b3833b703ab93be9d8c8848cbddbad
Diwakar <diwakar.singh313@gmail.com>
2023-12-21 19:12:40 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#F8F8F8",
+        gray: "#949494"
+      }
+    }
+  },
+  plugins: []
+};
