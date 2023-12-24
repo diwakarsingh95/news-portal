5fdecf4375f0ef29da286fac7f28fa5c84d63ada
Diwakar <diwakar.singh313@gmail.com>
2023-12-24 14:59:13 +0530

    footer margin

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="no-scrollbar">
       <Header />
-      <main className="max-w-xl sm:container mx-auto mt-20 px-6 sm:px-8 md:px-10 lg:px-20 xl:px-24 mb-10">{children}</main>
+      <main className="max-w-xl sm:container mx-auto mt-20 px-6 sm:px-8 md:px-10 lg:px-20 xl:px-24 mb-16">{children}</main>
       <Footer />
     </div>
   );
