8b5c1806ffde37f27d7cc59858361d8ed32880bd
Diwakar <diwakar.singh313@gmail.com>
2023-12-24 15:07:57 +0530

    updated hot topics skeleton style

--- src/components/HotTopics/HotTopicsSkeleton.tsx
+++ src/components/HotTopics/HotTopicsSkeleton.tsx
@@ -1,7 +1,7 @@
 const HotTopicsSkeleton = () => {
   return (
-    <div className="relative h-[200px] sm:h-[300px] xl:h-[400px] animate-pulse flex space-x-4">
-      <div className="h-full w-full rounded-lg bg-slate-400">
+    <div className="relative h-[200px] sm:h-[250px] md:h-[220px] lg:h-[276px] xl:h-[348px] 2xl:h-[400px] animate-pulse flex space-x-4">
+      <div className="h-full w-full rounded-lg mx-auto bg-slate-400">
         <p className="sr-only">Loading...</p>
       </div>
     </div>
